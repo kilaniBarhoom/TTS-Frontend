2c1053dceabcecd007ad936e2ec36c8cd1a7f6f8
kilaniBarhoom <ibrahim-kelani@hotmail.com>
2024-04-21 15:12:06 +0200

    Updated constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,9 @@
-export const loginEndp = import.meta.env.VITE_LOGIN_ENDPOINT;
-export const registerEndp = import.meta.env.VITE_REGISTER_ENDPOINT;
-export const refreshEndp = import.meta.env.VITE_REFRESH_TOKEN_ENDPOINT;
-export const logoutEndp = import.meta.env.VITE_LOGOUT_ENDPOINT;
-export const searchProjEndp = import.meta.env.VITE_SEARCH_PROJECTS_ENDPOINT;
-export const getProjEndp = import.meta.env.VITE_GET_PROJECT_BY_ID_ENDPOINT;
+export const loginEndp = import.meta.env.VITE_LOGIN_ENDPOINT as string;
+export const registerEndp = import.meta.env.VITE_REGISTER_ENDPOINT as string;
+export const refreshEndp = import.meta.env
+  .VITE_REFRESH_TOKEN_ENDPOINT as string;
+export const logoutEndp = import.meta.env.VITE_LOGOUT_ENDPOINT as string;
+export const searchProjEndp = import.meta.env
+  .VITE_SEARCH_PROJECTS_ENDPOINT as string;
+export const getProjEndp = import.meta.env
+  .VITE_GET_PROJECT_BY_ID_ENDPOINT as string;
