0db20ad82832bee0f4bd509b63a96ad761aa8b1a
kilaniBarhoom <ibrahim-kelani@hotmail.com>
2024-04-27 11:35:29 +0300

    fix(user-avatar): description under name

--- src/components/component/user-avatar.tsx
+++ src/components/component/user-avatar.tsx
@@ -4,19 +4,24 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 type UserAvatarProps = {
   name: string;
   image?: string;
-  email?: string;
+  description?: string;
 };
-const UserAvatar = ({ name, email }: UserAvatarProps) => {
+const UserAvatar = ({ name, description }: UserAvatarProps) => {
   const initials = getInitialsOfFullName(name);
+
   return (
     <div className="flex gap-2 ">
       <Avatar>
         <AvatarImage src={undefined} alt={name} />
         <AvatarFallback>{initials.toUpperCase()}</AvatarFallback>
       </Avatar>
-      <div className="flex flex-col items-start justify-center gap-[-20px]">
-        <p className='leading-none" text-sm font-medium'>{name}</p>
-        <p className="text-xs leading-none text-muted-foreground">{email}</p>
+      <div className="flex flex-col items-start justify-center gap-1">
+        <p className="leading-none text-foreground text-sm font-medium">
+          {name}
+        </p>
+        <p className="text-xs leading-none text-muted-foreground">
+          {description}
+        </p>
       </div>
     </div>
   );
