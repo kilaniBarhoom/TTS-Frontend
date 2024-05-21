894c04e769ec54c2a321ab03e475cb408bd5411e
kilaniBarhoom <ibrahim-kelani@hotmail.com>
2024-05-21 11:33:21 +0300

    refactor: Improve UI of NotificationsCard and Popover components

--- src/components/navbar/notifications/notifications.tsx
+++ src/components/navbar/notifications/notifications.tsx
@@ -20,7 +20,7 @@ export default function NotificationsCard({ className, ...props }: CardProps) {
   return (
     <Card className={cn("border-none w-full p-0", className)} {...props}>
       <CardHeader className="m-0 p-0">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between px-4 py-2">
           <Typography
             as={"largeText"}
             element="h5"
@@ -34,8 +34,8 @@ export default function NotificationsCard({ className, ...props }: CardProps) {
           </Button>
         </div>
       </CardHeader>
-      <Separator className="w-full border-border mt-2 mb-6" />
-      <CardContent className="grid gap-4 w-full p-0">
+      <Separator className="w-full border-border" />
+      <CardContent className="grid gap-4 w-full p-4">
         <ScrollArea className="h-52">
           <div>
             {isLoading ? (
--- src/components/navbar/notifications/popover.tsx
+++ src/components/navbar/notifications/popover.tsx
@@ -13,7 +13,8 @@ export function NotificationsPopover({
   return (
     <Popover>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
-      <PopoverContent className="w-[320px] ml-40 border-border">
+      <PopoverContent className="w-[320px] mt-[0.2rem] border-border relative  p-0">
+        <div className="absolute -top-4 left-1/2 -translate-x-1/2 transform  w-0 h-0 border-solid border-8 border-r-transparent border-l-transparent border-t-transparent border-b-border" />
         <NotificationsCard />
       </PopoverContent>
     </Popover>
