4d1951d871a4bb008818beafad9d155780908641
kilaniBarhoom <ibrahim-kelani@hotmail.com>
2024-05-23 11:23:48 +0300

    refactor: Update constants.ts with ticket-related endpoints

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -30,3 +30,11 @@ export const addMemToProjEndp = import.meta.env
 
 export const getMemNotifsEndp = import.meta.env
   .VITE_GET_MEMBER_NOTIFICATIONS_ENDPOINT as string;
+
+export const getTicketsByProjectId = import.meta.env
+  .VITE_GET_TICKETS_BY_PROJECTID_ENDPOINT;
+export const getTicketById = import.meta.env.VITE_GET_TICKETS_BY_ID_ENDPOINT;
+
+export const addCommentEndp = import.meta.env.VITE_ADD_COMMENT_ENDPOINT;
+export const editCommentEndp = import.meta.env.VITE_EDIT_COMMENT_ENDPOINT;
+export const deleteCommentEndp = import.meta.env.VITE_DELETE_COMMENT_ENDPOINT;
