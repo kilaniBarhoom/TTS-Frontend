f199b5f704494808a389cdb5524577adea06c473
kilaniBarhoom <ibrahim-kelani@hotmail.com>
2024-05-23 12:04:26 +0300

    refactor: Add TiptapEditor component for rich text editing
    - Create TiptapEditor component for integrating the Tiptap editor library
    - Include necessary dependencies and extensions for basic editor functionality
    - Implement menu bar with options for bold, italic, strikethrough, code, heading, and bullet list
    - Add support for toggling formatting options in the editor
    - Update styles for the TiptapEditor and MenuBar components

---
+++ src/components/tiptap-editor/index.tsx
@@ -0,0 +1,35 @@
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import MenuBar from "./menubar";
+
+export default function TiptapEditor({
+  content,
+  onChange,
+  error,
+}: {
+  content: string;
+  onChange: (richText: string) => void;
+  error?: boolean;
+}) {
+  const editor = useEditor({
+    extensions: [StarterKit.configure({})],
+    content: content,
+    editorProps: {
+      attributes: {
+        class: `rounded-md rounded-t-none outline-0 focus:ring-2 focus:ring-ring  min-h-[200px] p-2 text-foreground ${
+          error && "ring-2 ring-destructive/50"
+        }`,
+      },
+    },
+    onUpdate: ({ editor }) => {
+      onChange(editor.getHTML());
+    },
+  });
+
+  return (
+    <div className="grid border border-border rounded-md">
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
+}
---
+++ src/components/tiptap-editor/menubar.tsx
@@ -0,0 +1,74 @@
+import { type Editor } from "@tiptap/react";
+import { Bold, Code, Strikethrough, Heading, Italic, List } from "lucide-react";
+import { Toggle } from "@/components/ui/toggle";
+
+type MenuBarProps = {
+  editor: Editor | null;
+};
+
+const MenuBar = ({ editor }: MenuBarProps) => {
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <div className="flex border-b p-2 border-border gap-2">
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("bold")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleBold().run();
+        }}
+      >
+        <Bold size={20} />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("italic")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleItalic().run();
+        }}
+      >
+        <Italic size={20} />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("strike")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleStrike().run();
+        }}
+      >
+        <Strikethrough size={20} />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("code")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleCode().run();
+        }}
+      >
+        <Code size={20}></Code>
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("heading")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleHeading({ level: 1 }).run();
+        }}
+      >
+        <Heading size={20} />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("bulletList")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleBulletList().run();
+        }}
+      >
+        <List size={20} />
+      </Toggle>
+    </div>
+  );
+};
+
+export default MenuBar;
