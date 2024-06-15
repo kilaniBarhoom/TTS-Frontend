18e21031ac550c79adfad96b94bd21f3183c34cc
kilaniBarhoom <ibrahim-kelani@hotmail.com>
2024-06-15 17:39:39 +0300

    refactor: Update Auth Layout component
    - Removed the import statement for the `Header` component.
    - Added the `Typography` component to handle text rendering.
    - Updated the layout structure to use CSS grid for better alignment.
    - Adjusted the card styling and header background color.
    
    Closes #123

--- src/features/pages/Auth/Layout.tsx
+++ src/features/pages/Auth/Layout.tsx
@@ -1,10 +1,11 @@
-import Header from "@/components/navbar/header";
+// import Header from "@/components/navbar/header/header";
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Typography from "@/components/ui/typography";
 import { useTranslation } from "react-i18next";
 import { Outlet, useLocation } from "react-router-dom";
 
@@ -17,21 +18,16 @@ const Layout = () => {
       ? t("Enter your email below to register for an account")
       : t("Enter your email below to login to your account");
   return (
-    <div className="w-screen h-screen bg-background  flex flex-col">
-      <header className="flex items-center gap-4 border-b border-border bg-muted/40">
-        <Header />
-      </header>
-      <div className="flex flex-col items-center justify-center m-auto">
+    <div className="w-screen h-screen bg-background grid place-items-center">
+      <div className="flex flex-col items-center justify-center size-fit gap-0">
+        <div className="flex items-center justify-center gap-2 mb-4">
+          <img src="/assets/globals/TTSLogo.png" className="w-16" alt="TTS" />
+          <Typography element="h3" as={"h1"}>
+            TTS
+          </Typography>
+        </div>
         <Card className="w-full max-w-sm border border-border bg-background">
-          <div className=" relative">
-            <img
-              src="/assets/globals/TTSLogo.png"
-              className="w-24 absolute -top-10 left-1/2 -translate-x-1/2"
-              alt="TTS"
-            />
-          </div>
-
-          <CardHeader className="mb-4">
+          <CardHeader className="bg-transparent">
             <CardTitle className="text-2xl flex items-center justify-between">
               {cardTitle}
             </CardTitle>
